e5b39e07091600cf54c264a3bfb36d4ce4f23a16
Phil Kunz <gitlab@philkunz.com>
2019-07-07 22:57:12 +0200

    fix(core): update

--- ts/interfaces/csvparser.ts
+++ ts/interfaces/csvparser.ts
@@ -1,4 +1,5 @@
 export abstract class AcCsvParser<T> {
+  public abstract paymentProviderName: string;
   public abstract transactionArray: T[];
   public abstract async getTransactions(): Promise<T[]>;
 }
