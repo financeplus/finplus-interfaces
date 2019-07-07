7afcabf3f9eb098bfc5d6cf73ee160e330fd68fd
Phil Kunz <gitlab@philkunz.com>
2019-07-07 17:24:29 +0200

    fix(core): add Base class for csv Parsers

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,18 @@
 import { expect, tap } from '@pushrocks/tapbundle';
 import * as finplusInterfaces from '../ts/index';
 
+interface ITestTransaction {
+  date: Date;
+  amount: number;
+}
+
 tap.test('first test', async () => {
-  let dummyCsvParser : finplusInterfaces.ICsvParser;
-  dummyCsvParser;
+  class MyCsvParser extends finplusInterfaces.AcCsvParser<ITestTransaction> {
+    public transactionArray: ITestTransaction[] = [];
+    public async getTransactions(): Promise<ITestTransaction[]> {
+      return this.transactionArray;
+    }
+  }
 });
 
 tap.start();
